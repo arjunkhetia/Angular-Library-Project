84023ab5341f47571bd3cb2412b31aa9127623c1
Arjun Khetia <arjunkhetia@gmail.com>
2019-12-21 20:27:20 +0530

    Angular Library Project - 8.2.14 - Template

---
+++ projects/angular-library/src/lib/angular-library.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-Angular-Library',
+  template: `
+    <p>
+      Angular-Library works!
+    </p>
+  `,
+  styles: []
+})
+export class AngularLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
